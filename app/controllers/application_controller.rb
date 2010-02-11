28e68faab764455bb247993d7cb1a89ae248770d
Ivan Sidarau <rilley_elf@xpbyminl1577.minsk.epam.com>
2010-02-11 14:38:24 +0200

    adding google map

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 
+
+# google api key: chavanga.heroku.com: 'ABQIAAAAshX1rAk9Jx09XvfOOnHuVBQMTomGlBTa1OkUsqCzpm816lUX3xRvBtPiXkn3hacxzl4krqAa9fr7KA'
+
+require 'rubygems'
+require 'google_geocode'
+
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-  before_filter :load_events, :load_partner_links
+  before_filter :load_events, :load_partner_links, :load_google_map
   
   def load_events
     @left_column_events = Event.all :limit => 5, :conditions => { :published => true }
@@ -13,6 +19,14 @@ class ApplicationController < ActionController::Base
 
   def load_partner_links
     @left_column_partner_links = PartnerLink.all( :conditions => { :visible => true } )
+  end
+  
+  def load_google_map
+    application_id = 'ABQIAAAAshX1rAk9Jx09XvfOOnHuVBQMTomGlBTa1OkUsqCzpm816lUX3xRvBtPiXkn3hacxzl4krqAa9fr7KA'
+    @map = GMap.new(application_id)
+    @map.control_init(:small_map => true,:map_type => true)
+    @map.center_zoom_init([75.5,-42.56],4)
+#    @map.overlay_init(GMarker.new([75.6,-42.467],:title => "Chavanga Map", :info_window => "See Chavanga Map"))
   end
 
   def go_home
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,19 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+logo_image_change_interval = 5000;
+logo_image_size = 12;
+logo_pictures = new Array();
+for( i = 0 ; i < logo_image_size ; i++)
+{
+    logo_pictures[i] = new Image()
+    logo_pictures[i].src = '/images/logo/1A-'+(i+1)+'.jpg';
+}
+
+function changeLogoImage(i)
+{
+    document.logo_image.src = logo_pictures[i].src;
+    if (i == logo_pictures.length - 1) i = -1;
+    nextImageFunc = "changeLogoImage( "+(i+1)+" );";
+    setTimeout( nextImageFunc, logo_image_change_interval );
+}
