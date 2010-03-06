b05282497d00f93dcb9682b837fedb3d09351c42
Ivan Sidarau <rilley_elf@XPBYMINL1577.local>
2010-03-06 17:32:23 +0200

    adding fishing gallery clicking changing pre-algorythm

--- app/helpers/fishing_helper.rb
+++ app/helpers/fishing_helper.rb
@@ -2,4 +2,7 @@ module FishingHelper
     def fishing_program_path( fp )
         '/fishing/'+fp.id.to_s
     end
+    def fishing_program_on_click( image_name )
+        'change_fishing_program_image(this, '+image_name+');'
+    end
 end
