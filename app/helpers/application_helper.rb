035febf8b7aee53b58a3face76c89185e6333d77
Ivan Sidarau <rilley_elf@XPBYMINL1577.local>
2010-03-18 01:17:39 +0200

    weather widget now connected to production environment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -54,4 +54,11 @@ module ApplicationHelper
                 </td>"
         
     end
+    
+    def weather_widget
+        if ENV['RAILS_ENV'] == 'production'
+            return '<div align="center"><script src="http://www.weatherforecastmap.com/weather100.php?zona=russia_murmansk"></script></div>'
+        end
+        return ""
+    end
 end
