3e7df59acd9038e83d9ee1c01595f8821cbce025
rilley_elf <ivan.sidarau@gmail.com>
2010-11-02 01:27:14 +0200

    adding auto blog reader

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,32 @@
+require 'rss/2.0'
+require 'open-uri'
+
 class HomeController < ApplicationController
+  private
+    def get_title doc
+        doc.elements.each("rss/channel/item/title") { |element| return element.text }
+    end
+    def get_link doc
+        doc.elements.each("rss/channel/item/link") { |element| return element.text }
+    end
+    def get_description doc
+        doc.elements.each("rss/channel/item/atom:summary") { |element| return element.text }
+    end
+    def get_data
+        response = open("http://blog.chavanga.com/feeds/posts/default?alt=rss").read
+        doc = REXML::Document.new( response )
+        title = get_title doc
+        link = get_link doc
+        description = get_description doc
+        [title, link, description]
+    end
+  public
+    def load_news_from_blog
+        @title, @link, @description = get_data
+        render :partial => 'news'
+    end
+  
+    def index
+    end
+
 end
