d3c92b803afad961819ffc2d5b5bb4ac5f58745d
rilley_elf <ivan.sidarau@gmail.com>
2010-11-06 15:51:31 +0200

    increasing additional spaces and \n symbols

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,10 +18,11 @@ module ApplicationHelper
     end
     def main_menu_button( _name, _controller_name )
         id = _name.split(" ").join("_")
-        result = "<div class='main_menu_button'>
-                    <div id='main_menu_td_"+id+"' class='main_menu_button_element'
-            onmouseover='main_menu_td_"+id+".setAttribute(\"class\",\"main_menu_button_element_mouse_over\");'
-            onmouseout='main_menu_td_"+id.to_s+".setAttribute(\"class\",\"main_menu_button_element\");' >"
+        result = 
+"<div class='main_menu_button'>" +
+    "<div id='main_menu_td_"+id+"' class='main_menu_button_element'"+
+    "onmouseover='main_menu_td_"+id+".setAttribute(\"class\",\"main_menu_button_element_mouse_over\");'"+
+    "onmouseout='main_menu_td_"+id.to_s+".setAttribute(\"class\",\"main_menu_button_element\");' >"
         result += main_menu_link_to( _name, { :controller => _controller_name } )
         result += "</div></div>";
     end
@@ -30,14 +31,14 @@ module ApplicationHelper
         _name = "FISHING"
         id = _name
         _controller_name = { :controller => :fishing }
-        result = "<div class='main_menu_button'>
-                    <div id='main_menu_td_"+id+"' class='main_menu_button_element'
-                    onmouseover='showSubMenu(\"fishing_programs\");main_menu_td_"+id+".setAttribute(\"class\",\"main_menu_button_element_mouse_over\");' 
-                    onmouseout='hideSubMenu(\"fishing_programs\");main_menu_td_"+id+".setAttribute(\"class\",\"main_menu_button_element\");'>"
+        result = "<div class='main_menu_button'>"+
+                    "<div id='main_menu_td_"+id+"' class='main_menu_button_element'"+
+                    "onmouseover='showSubMenu(\"fishing_programs\");main_menu_td_"+id+".setAttribute(\"class\",\"main_menu_button_element_mouse_over\");' "+
+                    "onmouseout='hideSubMenu(\"fishing_programs\");main_menu_td_"+id+".setAttribute(\"class\",\"main_menu_button_element\");'>"
         result += main_menu_link_to( _name, _controller_name )
-        result += "<div id='fishing_programs' class='drop_down_menu'>
-             <div class='drop_down_menu_element'>
-                <ul style='margin-left:-15px;'>"
+        result += "<div id='fishing_programs' class='drop_down_menu'>"+
+             "<div class='drop_down_menu_element'>"+
+                "<ul style='margin-left:-15px;'>"
         @menu_fishing_programs.each do |fp|
             result += "<li>"+ link_to( fp.title, fp, :class => 'drop_down_main_menu' )
                 fp.SubFishingPrograms.all(:conditions => { :visible => true } ).each do |sp|
@@ -45,8 +46,8 @@ module ApplicationHelper
                 end
             result += "</li>"
         end
-        result += "</ul>
-             </div></div></div></div>";
+        result += "</ul>"+
+             "</div></div></div></div>";
 
     end
 
@@ -54,13 +55,13 @@ module ApplicationHelper
         _name = "GALLERY"
         id = _name
         _controller_name = @gallery_group
-        result = "<div class='main_menu_button'>
-                    <div id='main_menu_td_"+_name+"' class='main_menu_button_element'
-                    onmouseover='showSubMenu(\"gallery_groups\");main_menu_td_"+id+".setAttribute(\"class\",\"main_menu_button_element_mouse_over\");' 
-                    onmouseout='hideSubMenu(\"gallery_groups\");main_menu_td_"+id+".setAttribute(\"class\",\"main_menu_button_element\");'>"
+        result = "<div class='main_menu_button'>"+
+                    "<div id='main_menu_td_"+_name+"' class='main_menu_button_element'"+
+                    "onmouseover='showSubMenu(\"gallery_groups\");main_menu_td_"+id+".setAttribute(\"class\",\"main_menu_button_element_mouse_over\");' "+
+                    "onmouseout='hideSubMenu(\"gallery_groups\");main_menu_td_"+id+".setAttribute(\"class\",\"main_menu_button_element\");'>"
         result += main_menu_link_to( _name, _controller_name )
-        result += "<div id='gallery_groups' class='drop_down_menu'>
-                    <div class='drop_down_menu_element'>"
+        result += "<div id='gallery_groups' class='drop_down_menu'>"+
+                    "<div class='drop_down_menu_element'>"
         result += render :partial => 'layouts/gallery_drop_down_menu', :object => @gallery_group
         result += "</div></div>"+
                 "</div></div>"
@@ -93,8 +94,7 @@ module ApplicationHelper
     <div id='photo_zoomer'>
     </div>
   </td></tr></table>
-</div>
-"
+</div>"
     end
     def show_banner( banner )
         result = link_to( banner.value, banner.href, :alt => banner.alt, :class => 'banner' )
